923efa0faac78d32480af6770b71928ac9744340
salonirana2719 <saloni.rana2719@gmail.com>
2024-08-12 12:55:03 +0530

    v

--- src/component/Product.js
+++ src/component/Product.js
@@ -4,7 +4,7 @@ import Button from "./Button";
 const Product = ({elem}) => {
   return (
     <div className="w-full py-20   text-white ">
-      <div className="max-w-screen-xl mx-auto flex items-center justify-between   p-8">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between bg-slate-500  p-8">
         <h1 className="text-6xl capitalize font-medium" >{elem.title}</h1>
         <div className="details w-1/3 ">
           <p className="mb-10">
